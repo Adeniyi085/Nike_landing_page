e772ca702579a1278d65a320095354d58c18927a
Oluwatobi Adegelu <adeniyi.adegelu@gmail.com>
2023-08-22 00:20:38 +0100

    Major sections of the web application is created

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='relative'>
+      Nav 
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        Heero
+      </section>
+      <section className='padding'>
+        Popular products
+      </section>
+      <section className='padding'>
+        superQuality
+      </section>
+      <section className='padding-x py-10'>
+      Services
+      </section>
+      <section className='padding'>
+        Special offer
+      </section>
+      <section className='bg-pale-blue padding'>
+        customer Reviews
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        subscribe 
+      </section>
+      <section className='bg-black padding-x paddin-t pb-8'>
+        footer
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,9 @@
+const CustomerReviews = () => {
+  return (
+    <div>
+      CustomerReview
+    </div>
+  )
+}
+
+export default CustomerReviews
