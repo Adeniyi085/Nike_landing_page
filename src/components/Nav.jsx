e873b56739fa77c296924da52cafc1a3b43c9e44
Oluwatobi Adegelu <adeniyi.adegelu@gmail.com>
2023-08-22 00:29:27 +0100

    Navitgation done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,27 @@
 import { headerLogo} from '../assets/images'
-import { hamburger } from '../assets/icons'
-
+// import { hamburger } from '../assets/icons/hamburger.svg'
+import {navLinks } from '../constants'
 const Nav = () => {
   return (
     <header className='padding-x py-8 absolute z-10 w-full'> 
-        <nav>
+        <nav className='flex justify-between items-center max-container '>
             <a href="/">
                 <img src={headerLogo} alt="logo" 
                 width={130}
                 height={29}
                 />
             </a>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                {navLinks.map((item) => (
+                    <li key={item.label}>
+                        <a href={item.href} 
+                        className='font-montserrat leading-normal
+                        text-lg text-slate-gray'>
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </nav>
     </header>
   )
